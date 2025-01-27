9191633388aac706b22b7c41b933de5c617924c6
mahfoozalamcse <148795525+mahfoozalamcse@users.noreply.github.com>
2025-01-27 09:56:30 +0000

    selection sort practice

--- SeletionSort.java
+++ SeletionSort.java
@@ -15,6 +15,22 @@ public class SeletionSort {
 
         }
     }
+
+    // // practice selection sort
+    // public static void selectionSort1(int arr[]){
+    //    for (int i = 0; i < arr.length; i++) {
+    //       int minIdx = i;
+    //       for (int j = i+1; j < arr.length; j++) {
+    //           if (arr[minIdx] > arr[j]) {
+    //               minIdx = j;
+    //           }
+    //       }
+    //       int temp = arr[minIdx];
+    //       arr[minIdx] = arr[i];
+    //       arr[i] = temp;
+    //    }
+    // }
+
     public static void printArray(int arr[]){
       for (int i = 0; i < arr.length; i++) {
         System.out.print(arr[i]+" ");
